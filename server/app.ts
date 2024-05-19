1376930b2e8755b8da80fb39642c745ac996cf94
Yasindu Samarasinghe <yasindusamarasinghe10@gmail.com>
2024-05-19 22:05:43 +0530

    converting to rest api

--- server/app.ts
+++ server/app.ts
@@ -1,39 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import {
-    GoogleGenerativeAI,
-} from "@google/generative-ai";
+import express, { Express } from 'express'
+const app: Express = express()
 
-import axios from 'axios'
+import { getResult } from './controllers/ai-controller'
 
-const API_KEY: string | undefined = process.env.API_KEY;
-const genAI = new GoogleGenerativeAI(API_KEY ? API_KEY : '');
+app.get('/api/v1/ai/', getResult)
 
-const fetchImageAsBase64 = async (url: string): Promise<string> => {
-    try {
-        const response = await axios.get(url, { responseType: 'arraybuffer' });
-        return Buffer.from(response.data, 'binary').toString('base64');
-    } catch (error) {
-        console.error('Error fetching the image:', error);
-        throw error;
-    }
-}
-
-const run = async (): Promise<void> => {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-    const prompt = "what is this image include";
-    const imageBase64 = await fetchImageAsBase64('https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D')
-
-    const image = {
-        inlineData: {
-            data: imageBase64,
-            mimeType: "image/png",
-        },
-    };
-
-    const result = await model.generateContent([prompt, image]);
-    console.log(result.response.text());
-}
-
-run();
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`server is listening to port `);
+})
---
+++ server/controllers/ai-controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express'
+
+const getResult = async (): Promise<void> => {
+
+}
+
+// import {
+//     GoogleGenerativeAI,
+// } from "@google/generative-ai";
+// import axios from 'axios'
+
+// const API_KEY: string | undefined = process.env.API_KEY;
+// const genAI = new GoogleGenerativeAI(API_KEY ? API_KEY : '');
+
+// const fetchImageAsBase64 = async (url: string): Promise<string> => {
+//     try {
+//         const response = await axios.get(url, { responseType: 'arraybuffer' });
+//         return Buffer.from(response.data, 'binary').toString('base64');
+//     } catch (error) {
+//         console.error('Error fetching the image:', error);
+//         throw error;
+//     }
+// }
+
+// const run = async (): Promise<void> => {
+//     const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+//     const prompt = "what is this image include";
+//     const imageBase64 = await fetchImageAsBase64('https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D')
+
+//     const image = {
+//         inlineData: {
+//             data: imageBase64,
+//             mimeType: "image/png",
+//         },
+//     };
+
+//     const result = await model.generateContent([prompt, image]);
+//     console.log(result.response.text());
+// }
+
+// run();
+
+export { getResult }
